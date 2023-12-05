822de6478d8244a66c260d6582f3ae781ff22190
Emon <emonhasan7650@gmail.com>
2023-12-06 01:37:21 +0600

    AcademicFaculty controller, services and routes created

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod';
 
-const academicFacultyValidationSchema = z.object({
-  name: z
-    .string({
-      invalid_type_error: 'Academic Faculty must be string',
-    }),
+const createAcademicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Faculty must be string',
+  }),
+});
+
+const updateAcademicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Faculty must be string',
+  }),
 });
 
 export const AcademicFacultyValidation = {
-  academicFacultyValidationSchema,
+  createAcademicFacultyValidationSchema,
+  updateAcademicFacultyValidationSchema
 };
