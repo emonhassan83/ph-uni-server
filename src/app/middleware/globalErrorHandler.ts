deb8863e21883d5da556c83c36543c5b9b8b580f
Emon <emonhasan7650@gmail.com>
2023-12-08 02:21:55 +0600

    convert zod error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,16 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong';
-  
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      error: err,
-    })
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // setting default value
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
   }
 
-  export default globalErrorHandler;
+  // ultimate return
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSource,
+    stack: config.node_env === 'development' ? err?.stack : null,
+  });
+};
+
+export default globalErrorHandler;
